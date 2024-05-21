32057c34034565d6d2d19872d6a4ddaa7b6dc05c
JoshuaAisien <joshaisien@gmail.com>
2024-05-21 12:13:24 -0100

    new file created

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,8 +1,10 @@
 class Rectangle:
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -49,3 +51,7 @@ class Rectangle:
 
     def __repr__(self):
         return f"Rectangle({self.__width},{self.__height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
