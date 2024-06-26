abbb56f642ecd5c8ffe9f7ed7f46995a311417b8
JoshuaAisien <joshaisien@gmail.com>
2024-06-26 22:01:14 -0100

    creat file

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- create the user if it does not exit
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+--Apply the changes
+FLSUH PRIVILEGES;
